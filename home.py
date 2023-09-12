b9b86e431c0b2d05a8da7d1ce3b74d99b00df8af
Ifera3 <144359377+Ifera3@users.noreply.github.com>
2023-09-12 13:04:37 -0700

    Update home.py

--- home.py
+++ home.py
@@ -1,5 +1,36 @@
 import turtle
 
-s = turtle.getscreen
-t = turtle.Turtle
+s = turtle.getscreen()
+t = turtle.Turtle()
 
+t.penup()
+t.goto(-100, -100)
+t.pendown()
+for i in range(4):
+    t.fd(200)
+    t.lt(90)
+
+t.penup()
+t.goto(110, 90)
+t.lt(135)
+t.pendown()
+t.fd(156)
+t.lt(90)
+t.fd(156)
+t.penup()
+t.home()
+t.lt(90)
+t.pendown()
+for h in range(4):
+    t.fd(60)
+    t.lt(90)
+
+t.fd(30)
+t.lt(90)
+t.fd(60)
+t.rt(180)
+t.fd(30)
+t.rt(90)
+t.fd(30)
+t.rt(180)
+t.fd(60)
